6515ae7fe4146bff53fc9fb5efd557860b1e6505
zheng <2642198391@qq.com>
2023-03-21 13:25:44 +0800

    feat（util）：vite环境下图片加载工具

--- src/utils/img/imgInVite.ts
+++ src/utils/img/imgInVite.ts
@@ -1,3 +1,6 @@
 export const returnImg = (item:string)=>{
     return new URL(`/src/assets/img/${item}`, import.meta.url).href
 }
+export const returnIcon = (item:string)=>{
+    return new URL(`/src/assets/icon/${item}`, import.meta.url).href
+}
