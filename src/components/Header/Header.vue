dbf0a0c002720ebf79484e3691172ce71db85f86
zheng <2642198391@qq.com>
2023-03-21 23:04:06 +0800

    feat(conpoment):新增header组件

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -7,8 +7,8 @@
                         <el-menu-item>Sherrynan</el-menu-item>
                     </el-menu>
                     <el-menu mode="horizontal" class="right-box">
-                        <el-menu-item>首页<span></span></el-menu-item>
-                        <el-menu-item>记录</el-menu-item>
+                        <el-menu-item><router-link to="/">首页</router-link><span></span></el-menu-item>
+                        <el-menu-item><router-link :to="{name:'daily'}">记录</router-link></el-menu-item>
                         <el-menu-item>文档</el-menu-item>
                         <el-menu-item>关于作者</el-menu-item>
 
