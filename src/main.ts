25343163a9dc079d9af1a784ed1d72643188c22f
zheng <2642198391@qq.com>
2023-03-23 15:09:01 +0800

    chore：引入ant等库

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,22 @@ import 'element-plus/dist/index.css'
 import './design/index.less'
 import router from './router'
 
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
+import VueMarkdownEditor from '@kangc/v-md-editor';
+import '@kangc/v-md-editor/lib/style/base-editor.css';
+import vuepressTheme from '@kangc/v-md-editor/lib/theme/vuepress.js';
+import '@kangc/v-md-editor/lib/theme/style/vuepress.css';
+
+import Prism from 'prismjs';
+
+VueMarkdownEditor.use(vuepressTheme, {
+  Prism,
+});
 
 createApp(App)
 .use(ElementPlus)
+.use(Antd)
 .use(router)
+.use(VueMarkdownEditor)
 .mount('#app')
