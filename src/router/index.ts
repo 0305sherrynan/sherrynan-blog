684ecec2dc581e25b42fe8144c211cec2123087a
zheng <2642198391@qq.com>
2023-03-23 15:06:28 +0800

    feat（router）：路由入口

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,28 +1,45 @@
-import {createRouter,createWebHashHistory,RouteRecordRaw} from 'vue-router'
+import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
 
 //引入路由组件
 // import login from '../pages/login/index.vue'
 
-const routes:RouteRecordRaw[] = [
+const routes: RouteRecordRaw[] = [
     {
-        path:'/',
-        name:'home',
-        component:()=>import('@/views/Master/Master.vue'),
+        path: '/',
+        name: 'layout',
+        component: () => import('@/views/blog/layout/index.vue'),
         // meta:{
         //     requiresAuth:true
         // }
-        children:[
-
+        children: [
+            {
+                path: 'home',
+                name: 'master',
+                component: () => import('@/views/blog/Master/Master.vue'),
+            },
+            {
+                path: '/daily',
+                name: 'daily',
+                component: () => import('../views/blog/daily/Daily.vue')
+            },
         ]
     },
-    {    
-        path:'/daily',
-        name:'daily',
-        component:()=>import('../views/daily/Daily.vue')
+
+    {
+        path: '/admin',
+        name: 'admin',
+        component: () => import('@/views/admin/layout/index.vue'),
+        children: [
+            {
+                path:'addDaily',
+                name:'addDaily',
+                component:() => import('@/components/Daily/AddnewDaily.vue')
+            }
+         ]
     }
 ]
-const router =  createRouter({
+const router = createRouter({
     routes,
-    history:createWebHashHistory()
+    history: createWebHashHistory()
 })
 export default router
