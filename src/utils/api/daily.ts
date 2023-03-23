321f6d8c68e8fd0237991f5b453d497aa117986b
zheng <2642198391@qq.com>
2023-03-23 15:01:05 +0800

    feat （utils->api.->daily.ts）：封装daily请求接口

--- src/utils/api/daily.ts
+++ src/utils/api/daily.ts
@@ -1,4 +1,9 @@
 import { requestInstance } from "@/libs/axios";
+import {dailyApi} from '#/api'
 export const  getDailyInfo = ()=>{
-    return requestInstance.get("zwz/api/daily")
+    return requestInstance.get<any,dailyApi.getRequestResponse<Daily.Daily_all>>("zwz/api/daily",{params:{user_id:'26421983'}})
+}
+
+export const submitOneDailyInfo = (data:Daily.Daily_Exclude_id)=>{
+    return requestInstance.post<any,dailyApi.postRequestResponse<{message:string}>>("zwz/api/daily",Object.assign(data,{user_id:'26421983'}))
 }
