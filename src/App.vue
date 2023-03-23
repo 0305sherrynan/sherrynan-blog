44de0a7f44d77e3e0c7981ab55c293e14494bdc3
zheng <2642198391@qq.com>
2023-03-23 15:08:34 +0800

    refactor ：重构 app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
-    <Layout></Layout>
+    <!-- <Layout></Layout>
+     -->
+     <router-view></router-view>
   </div>
 </template>
 
 <script setup lang="ts">
-import Layout from '@/views/layout/index.vue'
+
 </script>
 
 <style scoped>
