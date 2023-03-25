486d311e56c9c0d08c8097de57dbe3c999e28f1a
zheng <2642198391@qq.com>
2023-03-25 14:11:51 +0800

    feat （utils->time）：封装时间相关的工具

---
+++ src/utils/time/index.ts
@@ -0,0 +1,21 @@
+/**
+ * formatDateTime 将形如2023-03-22T13:12:42.000Z 改格式为2023-03-03 16:07:12
+ */
+
+    //
+
+    
+	export const addZero = (num:number)=> {
+	    return num < 10 ? '0' + num : num;
+	}
+	export const formatDateTime = (date : string)=> {
+	    const time = new Date(Date.parse(date));
+		time.setTime(time.setHours(time.getHours()));
+		const Y = time.getFullYear() + '/';
+		const M = addZero(time.getMonth() + 1) + '/';
+	    const D = addZero(time.getDate()) + ' ';
+		const h = addZero(time.getHours()) + ':';
+		const m =addZero(time.getMinutes()) + ':';
+		const s = addZero(time.getSeconds());
+		return Y + M + D + h + m + s;
+    }
