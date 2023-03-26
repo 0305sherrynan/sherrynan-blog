d81f7025022c13e004b2efcc64cc281f71e17570
zheng <2642198391@qq.com>
2023-03-26 23:33:58 +0800

    feat（types->global.d.ts）：新增article接口

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -7,3 +7,22 @@ declare namespace Daily{
     type Daily_Exclude_id = Pick<Daily_all,'description' | 'submittime'> 
 }
 
+
+declare namespace Article{
+    interface Article_all {
+        article_id:string
+        title: string
+        description: string
+        time: string
+        cover:string
+        info: string
+        sort: string
+        user_id:string
+    }
+    type article_Exclude_userid = Pick<Article_all,'description' | 'time' | 'info'| 'sort'| 'cover'| 'title' | 'article_id'> 
+    interface article_sort  {
+        sortName: string
+        sortCount:number
+    }
+}
+declare function  returnArticle_all():articleApi.getRequestResponse<Article.Article_all>
