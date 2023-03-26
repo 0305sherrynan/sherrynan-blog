34a438ed24b6ece30f42d3e28c574bea675bec5c
zheng <2642198391@qq.com>
2023-03-26 23:34:11 +0800

    feat（types->api.d.ts）：新增daily接口

--- src/types/api.d.ts
+++ src/types/api.d.ts
@@ -1,12 +1,28 @@
-export namespace dailyApi{
+export namespace dailyApi {
     //get 请求daily数据返回的数据格式
-    interface getRequestResponse<T>{
-        code:number,
-        data:Array<T>
+    interface getRequestResponse<T> {
+        code: number,
+        data: Array<T>
     }
     //post 请求daily数据返回的数据格式
-    interface postRequestResponse<T>{
-        code:number,
-        data:T
+    interface postRequestResponse<T> {
+        code: number,
+        data: T
+    }
+}
+
+
+
+
+export namespace articleApi {
+     
+    interface getRequestResponse<T> {
+        code: number,
+        data: Array<T>
+    }
+    //post 请求daily数据返回的数据格式
+    interface postRequestResponse<T> {
+        code: number,
+        data: T
     }
 }
