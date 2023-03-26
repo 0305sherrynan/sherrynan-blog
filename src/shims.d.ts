cc486cac252d5882d29288d12ad67595355940c9
zheng <2642198391@qq.com>
2023-03-26 23:36:49 +0800

    chore：库的必备声明文件 新增

--- src/shims.d.ts
+++ src/shims.d.ts
@@ -1,3 +1,5 @@
 declare module '@kangc/v-md-editor/lib/theme/vuepress.js';
 declare module '@kangc/v-md-editor';
-declare module 'prismjs'
+declare module 'prismjs';
+declare module '@kangc/v-md-editor/lib/theme/github.js'
+declare module '@kangc/v-md-editor/lib/preview-html'
