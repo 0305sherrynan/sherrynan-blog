22a6b749615031889810efc0b5c941b7c0a4a580
zheng <2642198391@qq.com>
2023-03-26 23:32:51 +0800

    doc （utils->api.->daily.ts）：加入注释

--- src/utils/api/daily.ts
+++ src/utils/api/daily.ts
@@ -1,5 +1,9 @@
 import { requestInstance } from "@/libs/axios";
 import {dailyApi} from '#/api'
+/**
+ * getDailyInfo 获取对应用户id的daily
+ * submitOneDailyInfo 提交单个daily到数据库
+ */
 export const  getDailyInfo = ()=>{
     return requestInstance.get<any,dailyApi.getRequestResponse<Daily.Daily_all>>("zwz/api/daily",{params:{user_id:'26421983'}})
 }
