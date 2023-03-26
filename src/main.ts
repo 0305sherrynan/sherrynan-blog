f59c5c2b757910bb940b43dfdffa6bcfaa53f3e9
zheng <2642198391@qq.com>
2023-03-26 23:37:11 +0800

    chore：md库

--- src/main.ts
+++ src/main.ts
@@ -11,11 +11,34 @@ import VueMarkdownEditor from '@kangc/v-md-editor';
 import '@kangc/v-md-editor/lib/style/base-editor.css';
 import vuepressTheme from '@kangc/v-md-editor/lib/theme/vuepress.js';
 import '@kangc/v-md-editor/lib/theme/style/vuepress.css';
+import githubTheme from '@kangc/v-md-editor/lib/theme/github.js';
+import '@kangc/v-md-editor/lib/theme/style/github.css';
+
+// main.js
+import VMdPreviewHtml from '@kangc/v-md-editor/lib/preview-html';
+import '@kangc/v-md-editor/lib/style/preview-html.css';
+
+
+
+
 
 import Prism from 'prismjs';
+// highlightjs 核心代码
+import hljs from 'highlight.js/lib/core';
+// 按需引入语言包
+import javascript from 'highlight.js/lib/languages/javascript';
 
+hljs.registerLanguage('javascript', javascript);
 VueMarkdownEditor.use(vuepressTheme, {
   Prism,
+});
+VueMarkdownEditor.use(githubTheme, {
+  Hljs: hljs,
+  config: {
+    toc: {
+      includeLevel: [2,3],
+    },
+  },
 });
 
 createApp(App)
@@ -23,4 +46,5 @@ createApp(App)
 .use(Antd)
 .use(router)
 .use(VueMarkdownEditor)
+.use(VMdPreviewHtml)
 .mount('#app')
