bcda77afd7f312d4778d5a8cb56d8a28db890e58
zheng <2642198391@qq.com>
2023-03-26 23:35:42 +0800

    feat(conpoment):新增banner组件

---
+++ src/components/Banner/Banner.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="banner" style="animation: header-effect 1s;">
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+.banner{
+    width: 100%;
+    height: 50vh;
+    background: url('@/assets/img/sky.jpg');
+    background-repeat: no-repeat;
+    background-size: 100% 100%;
+}
+</style>
