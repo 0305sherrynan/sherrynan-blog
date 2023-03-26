515ca98b2226661d3f79f1f5fe8b8c1bfc95df85
zheng <2642198391@qq.com>
2023-03-26 23:28:25 +0800

    feat（utils->views->article-detail）：文章视图

---
+++ src/views/blog/article-detail/index.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <Banner></Banner>
+        <div class="detail-box">
+           
+                <Suspense>
+                    <async-article></async-article>
+                </Suspense>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import Banner from '@/components/Banner/Banner.vue';
+
+import AsyncArticle from './AsyncArticle.vue'
+
+
+</script>
+
+<style scoped lang="less">
+.detail-box{
+    // padding: ;
+    display: flex;
+    flex-direction: row;
+    margin-top: 50px;
+    justify-content: center;
+
+
+}
+</style>
