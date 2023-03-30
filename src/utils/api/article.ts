4afa5b6949352d47455027b0ebccabd05d49a1fb
zheng <2642198391@qq.com>
2023-03-30 14:46:52 +0800

    feat （utils->api.->article.ts）：封装article请求接口

--- src/utils/api/article.ts
+++ src/utils/api/article.ts
@@ -2,17 +2,20 @@ import { requestInstance } from "@/libs/axios";
 import {articleApi} from '#/api'
 /**
  * getArticleInfo 获取对应用户id的article列表
+ * getArticleInfoBySort 获取对应用户在特定标签分类下的文章
  * submitOneArticleInfo 提交单个article到数据库
  * getArticleSortAndNumber 获取sort分类和对应数量
+ * getArticleByArticleid 获取article_id的文章
  */
 export const  getArticleInfo = ()=>{
     return requestInstance.get<any,articleApi.getRequestResponse<Article.Article_all>>("zwz/api/article",{params:{user_id:'26421983'}})
 }
-
+export const getArticleInfoBySort = (sortName:string)=>{
+    return requestInstance.get<any,articleApi.getRequestResponse<Article.Article_all>>(`zwz/api/article/sort/${sortName}`,{params:{user_id:'26421983'}})
+}
 export const submitOneArticleInfo = (data:Article.article_Exclude_userid)=>{
     return requestInstance.post<any,articleApi.postRequestResponse<{message:string}>>("zwz/api/article",Object.assign(data,{user_id:'26421983'}))
 }
-
 export const getArticleSortAndNumber = ()=>{
     return requestInstance.get<any,articleApi.getRequestResponse<Article.article_sort>>("zwz/api/article/sort",{params:{user_id:'26421983'}})
 }
