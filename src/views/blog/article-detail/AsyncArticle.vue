7f25f0b334bda835fb9c82239bc24776e0227c66
zheng <2642198391@qq.com>
2023-03-30 14:43:52 +0800

    feat（utils->views->article-detail）：j加入侧边导航栏

--- src/views/blog/article-detail/AsyncArticle.vue
+++ src/views/blog/article-detail/AsyncArticle.vue
@@ -1,48 +1,136 @@
 <template>
-    <!-- <span v-html=""></span> -->
-    <div class="left-article">
-        <v-md-preview-html :html="articleDetails[0].info" preview-class="github-markdown-body"></v-md-preview-html>
+  <!-- <span v-html=""></span> -->
+  <div class="left-article">
+    <v-md-preview-html :html="articleDetails[0].info" preview-class="github-markdown-body"
+      ref="preview"></v-md-preview-html>
+  </div>
+  <div class="right-toc" :class="{isFix:isScollEnough}">
+    <div style="border-bottom: 1px solid rgb(228, 230, 235);text-align: center;padding: 10px;"><span style="font-size: 14px;font-weight: 500;line-height: 20px;">目录</span></div>
+    <div v-for="anchor in title" :style="{ padding: `10px 0 10px ${anchor.indent * 20}px` }"
+      @click="handleAnchorClick(anchor)">
+      <a style="cursor: pointer" class="sidebar-a">{{ anchor.title }}</a>
     </div>
-    <div class="right-toc">
-        <!-- <div v-for="">
-            <a ></a>
-        </div> -->
-    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { getArticleInfo ,getArticleByArticleid} from '@/utils/api/article'
+import { getArticleInfo, getArticleByArticleid } from '@/utils/api/article'
+import { tr } from 'element-plus/es/locale';
+import { getCurrentInstance, onMounted, reactive, ref, watch } from 'vue';
 import { useRouter } from 'vue-router';
-
 /**
  * data
  * articleDetails 获取文章
  * tocTitle 导航的标题
  * router 获取路由
+ * re 正则表达式 用于将导航提取
+ * preview dom获取
+ * nowScrollPos 当前滚动的px
+ * isScollEnough 是否开启fix样式
+ */
+
+const router = useRouter()
+const articleDetails:Array<Article.Article_all> = await getArticleByArticleid(router.currentRoute.value.query.article_id as string).then((res) => {
+  return res.data
+})
+let title = reactive<any>([])
+let tocTitlestring = articleDetails[0].title
+//去除掉toc
+let re = /<p[\s\S]+<\/div><\/p>/g
+articleDetails[0].info = articleDetails[0].info.replace(re,'')
+
+let nowScrollPos = ref<number>(0)
+const isScollEnough = ref<boolean>(false)
+const scrollHandler = ()=>{
+  window.addEventListener('scroll',()=>{
+    //不要用document.body.scrollTop获取 DTD问题
+    nowScrollPos.value = document.documentElement.scrollTop
+    
+  })
+}
+watch(nowScrollPos,(newval,oldval)=>{
+  isScollEnough.value=true
+  // if (newval>500) isScollEnough.value = true
+  // else isScollEnough.value = false
+})
+/**
+ * 侧边栏
  */
 
 
-const router = useRouter()
-const { data: articleDetails } = await getArticleByArticleid(router.currentRoute.value.query.article_id as string)
-const tocTitle = articleDetails[0].title
-// console.log(router.currentRoute.value.query.article_id)
+const preview = ref()
 
-console.log(articleDetails)
+/**
+ * 当文章挂载后（挂载后才能使用$el） 获取侧边栏
+ */
+onMounted(async () => {
+  //挂载事件
+  scrollHandler()
+  const { proxy }: any = getCurrentInstance()
+
+  const anchors: Array<any> = proxy.$refs.preview.$el.querySelectorAll('h1,h2,h3,h4,h5,h6')
+  const titles: any = Array.from(anchors).filter((title: any) => !!title.innerText.trim())
+  const hTags = Array.from(new Set(titles.map((title: any) => title.tagName))).sort()
+  title = Object.assign(title, titles.map((el: any) => ({
+    title: el.innerText,
+    lineIndex: el.getAttribute('data-v-md-line'),
+    indent: hTags.indexOf(el.tagName),
+  })))
+})
+
+/**
+ * 
+ *  method
+ * handleAnchorClick 跳转指定位置
+ */
+const handleAnchorClick = (anchor: any) => {
+
+  const { lineIndex } = anchor;
+
+  const heading = preview.value.$el.querySelector(`[data-v-md-line="${lineIndex}"]`);
+
+  if (heading) {
+    preview.value.scrollToTarget({
+      target: heading,
+      scrollContainer: window,
+      top: 60,
+    });
+  }
+}
 </script>
 
-<style scoped>
-    .left-article{
-        border: 1px solid #ebeef5;
-        width: 700px;
-       
-        margin-right: 50px;
-        border-radius: 8px;
-        padding: 20px;
-    }
-    .right-toc{
-        border: 1px solid #ebeef5;
-        width: 300px;
-        height: 1000px;
-        border-radius: 8px;
-    }
+<style scoped lang="less">
+@media (max-width: 800px) {
+  .left-article{
+    max-width: 450px;
+  }
+  .right-toc{
+    max-width: 150px;
+  }
+}
+.left-article {
+  border: 1px solid #ebeef5;
+  width: 700px;
+
+  margin-right: 50px;
+  border-radius: 8px;
+  padding: 20px;
+}
+
+.right-toc {
+  border: 1px solid #ebeef5;
+  width: 300px;
+  height: 100%;
+  padding: 10px;
+  border-radius: 8px;
+  .sidebar-a{
+    color: black;
+  }
+}
+.isFix{
+  position: sticky;
+  top:30px;
+  // right: 10px;
+
+}
 </style>
