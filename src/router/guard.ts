518c9a14d7e866fc7bc07a2f9df60f66d56751bb
zheng <2642198391@qq.com>
2023-04-03 19:47:51 +0800

    feat（router->guard）：前置路由守卫

---
+++ src/router/guard.ts
@@ -0,0 +1,26 @@
+import router from './index'
+import { StorageHandler,StorageType } from '@/libs/storage'
+
+router.beforeEach((to,from,next)=>{
+    const storage = new StorageHandler()
+    //获取token
+    const token = storage.getItem(StorageType.Local,'token')
+    console.log(token)
+    //不需要token 直接通行
+    if (to.matched.some(value=>to.meta.freePass)) {
+        console.log(444)
+        next()
+    }
+    else { //需要token
+        //token为空，跳转登录页
+        if (token == null) {
+            console.log(123)
+            next({name:'entrance',params:{operate:'login'}})
+        }
+        //token有
+        else{
+            next()
+        }
+    }
+    // next()
+})
