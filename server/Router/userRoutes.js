59ebd4c48ee8b6764624fcb96a53712a049edd99
zheng <2642198391@qq.com>
2023-04-03 22:07:45 +0800

    feat（server->router）:userRouter

--- server/Router/userRoutes.js
+++ server/Router/userRoutes.js
@@ -3,15 +3,13 @@ const router = express.Router()
 const $sql = require('../Dao/impl/userImpl.js')
 const $jwt = require('../Utils/jwt.js')
 router.post('/login', (req, res) => {
-    // res.send({'zwz':'123'})
+  
     $sql.verifyLoginInfo(req, res)
 })
-router.get('/',$jwt.verify,(req,res,next)=>{
-    res.json({
-        code:200,
-        data:{
-            message:'token正确'
-        }
-    })
+router.post('/judgeRegisterd',(req,res)=>{
+    $sql.judgeEmailRegisted(req, res)
+})
+router.post('/register',(req,res)=>{
+    $sql.registerAccount(req,res)
 })
 module.exports = router
