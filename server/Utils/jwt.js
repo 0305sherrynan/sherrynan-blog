b1dd6a775076a1b7b08f74726f05b66591016d05
zheng <2642198391@qq.com>
2023-04-06 14:05:03 +0800

    style(utils工具类):规范

--- server/Utils/jwt.js
+++ server/Utils/jwt.js
@@ -12,17 +12,12 @@ module.exports =  class jwtUtil {
 
     static verify(req,res,next){
         const authorization = req.headers.authorization
-        // console.log(authorization)
-        // next()
         let token = ''
         if (authorization.includes('Bearer')){
             token = authorization.replace('Bearer ','')
         }else  token = authorization
-        console.log(token)
         jwt.verify(token,secretkey,(error,data)=>{
-            console.log(error,data)
             if (error) {
-                // console.log('error')
                 if (error.name == 'TokenExpiredError'){
                     res.json({
                         code:200,
@@ -39,16 +34,12 @@ module.exports =  class jwtUtil {
                         }
                     })
                 }
-                // next()
-                // return
             }else{
                 //验证通过，将带有身份认证的id（实际为登录的user_id） 赋值给req，
                 //则请求时不需要自己携带user_id
                 req._id = data._id
-
                 next()
             }
-            // next()
         })  
     }
 } 
