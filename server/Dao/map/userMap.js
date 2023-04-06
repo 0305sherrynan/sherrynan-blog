25b6b20624c2a7ed9375e7cb2aa09d17158cd82c
zheng <2642198391@qq.com>
2023-04-06 16:40:06 +0800

    fix（Dao->map):修复user插入新数据失败问题

--- server/Dao/map/userMap.js
+++ server/Dao/map/userMap.js
@@ -5,7 +5,7 @@ const userMap = {
     */
     verifyLoginInfo:'select * from user b where b.email = ? and b.password = ?',
     judgeEmailRegisted:'select * from user where user.email =?',
-    registerAccount:'insert into user values(?,?,?,?)',
+    registerAccount:'insert into user values(?,?,?,?,?)',
     getPersonInfo:'select nickname,email,password,coverimg from user where user_id = ?',
     modifyPersonInfo:'update user  set email = ?,password = ?,nickname = ?,coverimg = ? where user_id = ?',
     modifyPasInfo:'update user set password = ? where user_id = ?'
