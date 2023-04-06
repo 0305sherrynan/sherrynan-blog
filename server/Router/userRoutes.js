0e23e16f63d0082f2b41eb404f76f73b70c0d750
zheng <2642198391@qq.com>
2023-04-06 14:04:05 +0800

    feat（server->router）:userRouter

--- server/Router/userRoutes.js
+++ server/Router/userRoutes.js
@@ -12,4 +12,15 @@ router.post('/judgeRegisterd',(req,res)=>{
 router.post('/register',(req,res)=>{
     $sql.registerAccount(req,res)
 })
+router.get('/getInfo',$jwt.verify,(req,res)=>{
+    $sql.getPersonInfo(req,res)
+})
+router.post('/modifyInfo',$jwt.verify,(req,res)=>{
+    $sql.modifyPersonInfo(req,res)
+})
+router.post('/modifyPas',$jwt.verify,(req,res)=>{
+    $sql.modifyPasInfo(req,res)
+})
+
+
 module.exports = router
