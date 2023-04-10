e5b8c3549042d5f912ecc0025237af3eba3efca1
zheng <2642198391@qq.com>
2023-04-10 18:25:20 +0800

    feat （utils->judge）：封装判断是否实现对应接口的工具

---
+++ src/utils/judge/index.ts
@@ -0,0 +1,12 @@
+//以data下是否有message元素作为判断标准是否为有数据 返回true ：为有数据 
+export const DataTypeJudge = (data:unknown):data is {message:string} =>{
+    return (data as {message:string}).message == undefined
+}
+// export const DataTypeJudgeGenel = <T>(data:unknown,leixing:T):boolean =>{
+//     type key = keyof T
+//     return (data as T)[key] == undefined
+// }
+
+export const DataTypeJudgeParentID = (data:unknown):data is {parentid:string} =>{
+    return (data as {parentid:string}).parentid == undefined
+}
