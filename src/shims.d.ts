c6746d873daa447fad8caae03cfdff13c1150bff
zheng <2642198391@qq.com>
2023-04-10 18:29:59 +0800

    chore：库的必备声明文件 新增

--- src/shims.d.ts
+++ src/shims.d.ts
@@ -3,3 +3,4 @@ declare module '@kangc/v-md-editor';
 declare module 'prismjs';
 declare module '@kangc/v-md-editor/lib/theme/github.js'
 declare module '@kangc/v-md-editor/lib/preview-html'
+declare module 'emoji-mart-vue-fast/src'
