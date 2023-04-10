b1940aa3c2a2c58c56bfb3944d67f40e517007a3
zheng <2642198391@qq.com>
2023-04-10 18:26:26 +0800

    style：规范代码，删去无用代码

--- src/utils/api/user.ts
+++ src/utils/api/user.ts
@@ -7,7 +7,7 @@ import { da } from "element-plus/es/locale";
  * verifyLoginInfo 验证登录信息
  * judgeEmailRegisted 判断该email是否已被注册
  * registerAccount 注册账号
- * getPersonInfo 获取个人信息
+ * getPersonInfo 获取个人信息 可以修改成直接从storage取
  * modifyPersonInfo 修改个人信息 (全部修改)
  * modifyPasInfo 修改个人密码 
  */
@@ -16,11 +16,9 @@ export const verifyLoginInfo = (dataForm:{})=>{
     return requestInstance.post<any,userApi.postRequestResponse<{message:string,token:string}>>('zwz/api/user/login',dataForm)
 }
 export const judgeEmailRegisted = (email:string)=>{
-    // const storage = new StorageHandler
     return requestInstance.post<any,userApi.postRequestResponse<{message:string}>>('zwz/api/user/judgeRegisterd',{email})
 }
 export const registerAccount = (dataForm:{})=>{
-    // const storage = new StorageHandler
     return requestInstance.post<any,userApi.postRequestResponse<{message:string}>>('zwz/api/user/register',dataForm)
 }
 export const getPersonInfo = ()=>{
