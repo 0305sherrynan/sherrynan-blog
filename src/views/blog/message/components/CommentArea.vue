e0192015d6c6c2f841a431413037a79c0eaf14cf
zheng <2642198391@qq.com>
2023-04-10 18:34:12 +0800

    feat（utils->views->comment->components）：comment主视图

---
+++ src/views/blog/message/components/CommentArea.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="comment-area">
+        <send-message :userInfo="coverAndNick"></send-message>
+        <show-message :parentCom="allCommentInfo" ></show-message>
+    </div>
+</template>
+
+<script setup lang="ts">
+import SendMessage from '@/components/Comment/SendMessage.vue'
+import ShowMessage from '@/components/Comment/ShowMessage.vue'
+import { getParentCom } from '@/utils/api/comment'
+import { getPersonInfo } from '@/utils/api/user'
+
+/**
+ * coverAndNick 头像和昵称获取 后续改从storage获取
+ * messageAll 获取所有的主留言内容
+ */
+const {data:coverAndNick} = await getPersonInfo()
+const {data:allCommentInfo} = await getParentCom() 
+// const {data::sonMessage} = await getSonCom()
+
+</script>
+
+<style scoped lang="less">
+.comment-area {
+    width: 500px;
+    height: 1000px;
+    padding: 0 30px;
+}
+</style>
