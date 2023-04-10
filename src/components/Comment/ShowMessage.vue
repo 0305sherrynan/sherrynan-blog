9885f278e19a0905350a35621663523ccb919b25
zheng <2642198391@qq.com>
2023-04-10 18:35:52 +0800

    feat(conpoment->comment):评论展示组件

---
+++ src/components/Comment/ShowMessage.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="showmes-big-box">
+        <h2 style="font-weight: 600;">全部留言</h2>
+        <div class="showmes-box" v-for="(item, index) in props.parentCom" :key="index">
+            <single-message :singleInfo="{item,index,parentOrSon:true}">
+                <template #sonMessage>
+                    <Suspense>
+                        <show-son-message :parentID="item.id"></show-son-message>
+                    </Suspense>
+                </template>
+            </single-message>
+
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import SingleMessage from './SingleMessage.vue'
+import ShowSonMessage from './ShowSonMessage.vue'
+
+/**
+ * 
+ * allCommentInfo 所有的评论内容 先做一级的 后续做多级
+ * isShowComment 是否显示准备评论
+ */
+interface allCommentInfo {
+    parentCom: Array<Comment.getParentComent>
+}
+const props = defineProps<allCommentInfo>()
+
+</script>
+
+<style scoped lang="less">
+.showmes-big-box {
+    margin-top: 60px;
+}
+
+.showmes-box {
+    display: flex;
+    flex-direction: row;
+    margin-top: 20px;
+}
+
+</style>
