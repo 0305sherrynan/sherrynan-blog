43c79c6573f0a6dfdc9e68b1e332531ee97ab094
zheng <2642198391@qq.com>
2023-04-10 18:35:16 +0800

    feat(conpoment->comment):评论组件

---
+++ src/components/Comment/SendMessage.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="message-box">
+        <div>
+            <img :src="userInfo.coverimg?returnDBImg(userInfo.coverimg):returnDBImg('3mijx63g.png')" alt="" style="width: 50px;height: 50px;border-radius: 50%;">
+        </div>
+        <text-area :parentid="DataTypeJudgeParentID(userInfo)?userInfo.parentid:undefined"></text-area>
+        
+        <div>
+
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import { returnDBImg } from '@/utils/img/imgInVite'
+import TextArea from './TextArea.vue'
+import {commentApi} from '#/api'
+import {DataTypeJudgeParentID} from '@/utils/judge'
+
+interface coverAndNickInterface {
+    userInfo: User.get_personal_info | commentApi.getSonComentByid
+    
+}
+/**
+ * props
+ */
+const props = defineProps<coverAndNickInterface>()
+
+</script>
+
+<style scoped lang="less">
+.message-box{
+    display: flex;
+    flex-direction: row;
+
+}
+
+</style>
