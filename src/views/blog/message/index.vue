9b1999e1d620ee15c885aa0f80afee94324caa71
zheng <2642198391@qq.com>
2023-04-10 18:33:26 +0800

    feat（utils->views->comment）：新增留言板视图

---
+++ src/views/blog/message/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <Banner></Banner>
+
+        <div class="comment-layout">
+            <suspense>
+                <comment-area></comment-area>
+            </suspense>
+            <div style="width:300px;height:500px;background-color: antiquewhite;"></div>
+        </div>
+
+    </div>
+</template>
+
+<script setup lang="ts">
+import Banner from '@/components/Banner/Banner.vue'
+import CommentArea from './components/CommentArea.vue'
+</script>
+
+<style scoped lang="less">
+.comment-layout {
+    display: flex;
+    flex-direction: row;
+    margin-top: 50px;
+    justify-content: space-evenly;
+}
+</style>
